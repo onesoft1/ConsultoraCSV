8be87328281d3e00328098c2ab28c15338ee2889
Garona <cenicienta.kel@gmail.com>
2018-07-04 13:51:46 -0400

    hiu1

---
+++ app/src/main/java/com/example/maury/csv/DescripcionAmbientes.java
@@ -0,0 +1,13 @@
+package com.example.maury.csv;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DescripcionAmbientes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_descripcion_ambientes);
+    }
+}
